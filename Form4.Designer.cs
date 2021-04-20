ed4fab7a6e90283a5197f67970fca17941e53508
Gusso0o <73793709+Gusso0o@users.noreply.github.com>
2021-04-20 12:45:06 +0200

    Added v 1.1

---
+++ Form4.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace IP_Finder_
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.myIPAddress = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show My LAN IP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "(Hidden)";
+            // 
+            // myIPAddress
+            // 
+            this.myIPAddress.AutoSize = true;
+            this.myIPAddress.Location = new System.Drawing.Point(169, 65);
+            this.myIPAddress.Name = "myIPAddress";
+            this.myIPAddress.Size = new System.Drawing.Size(63, 17);
+            this.myIPAddress.TabIndex = 4;
+            this.myIPAddress.Text = "(Hidden)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Show My Public IP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 105);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.myIPAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form4";
+            this.Text = "IP Finder®";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label myIPAddress;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Form5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP_Finder_
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ping Ping = new Ping();
+            PingReply reply = Ping.Send(textBox1.Text, 1000);
+            MessageBox.Show(reply.Status.ToString());
+        }
+    }
+}
